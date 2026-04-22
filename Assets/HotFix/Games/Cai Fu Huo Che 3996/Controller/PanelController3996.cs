2eb8a14c693fc628c094af7bfaa2a440bb286cbc
Qinbum <954604479@qq.com>
2026-04-22 14:43:39 +0800

    提交

--- Assets/HotFix/Games/Cai Fu Huo Che 3996/Controller/PanelController3996.cs
+++ Assets/HotFix/Games/Cai Fu Huo Che 3996/Controller/PanelController3996.cs
@@ -11,7 +11,7 @@ using UnityEngine;
 public class PanelController3996 : SlotMaker.PanelBaseController
 {
     new SpinButtonController spinBtnCtrl = new SpinButtonController();
-    protected override string PanelPackagePath => "Assets/GameRes/Games/CaiFuHuoChe3996_Panel01/FGUIs";
+    protected override string PanelPackagePath => "Assets/GameRes/Panel/CaiFuHuoChe3996_Panel01/FGUIs";
 
 
     public override void Init(EventData res = null)
